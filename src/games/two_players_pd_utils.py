169f6b57ff99ac0cb71f4dd93d3ab1b2ef17a608
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-03-28 10:08:27 +0100

    Add function to plot history of the game.

--- src/games/two_players_pd_utils.py
+++ src/games/two_players_pd_utils.py
@@ -1,5 +1,11 @@
+import json
 import warnings
 
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+from src.utils import OUT_BASE_PATH
 
 action_0_ = "Defect"
 action_1_ = "Cooperate"
@@ -44,3 +50,69 @@ def two_players_pd_payoff(own_action: int, other_action: int) -> int:
         return 1
     else:
         raise ValueError("Invalid actions")
+
+
+def plot_history_analysis(timestamp, infix=None):
+    run_dir_path = OUT_BASE_PATH / str(timestamp)
+    history_file_path = run_dir_path / "game_history.json"
+    out_path = run_dir_path / "plots"
+    out_path.mkdir(exist_ok=True)
+
+    with open(history_file_path, "r") as history_file:
+        history = json.load(history_file)
+
+    A_history = history[player_1_]
+
+    A_n_coop = A_history.count(1)
+    A_coop_perc = A_n_coop / len(A_history)
+    A_n_defect = A_history.count(0)
+    A_defect_perc = A_n_defect / len(A_history)
+
+    # Histogram
+    plt.bar([f"Cooperate - {A_coop_perc}%", f"Defect - {A_defect_perc}%"], [A_n_coop, A_n_defect], color=['green', 'red'])
+    plt.title(f'Occurrences of "Cooperate" and "Defect" - {timestamp}')
+    plt.ylabel('Occurrences')
+    plt.tight_layout()
+    if infix is None:
+        plt.savefig(out_path / f"occurrences.svg")
+        plt.savefig(out_path / f"occurrences.png")
+    else:
+        plt.savefig(out_path / f"occurrences_{infix}.svg")
+        plt.savefig(out_path / f"occurrences_{infix}.png")
+    plt.show()
+
+    # Transition matrix
+    transition_matrix = np.zeros((2, 2), dtype=int)
+    for i in range(len(A_history) - 1):
+        transition_matrix[A_history[i], A_history[i + 1]] += 1
+
+    ax = sns.heatmap(transition_matrix, annot=True, xticklabels=[0, 1], yticklabels=[0, 1], cmap="Blues", fmt='d')
+    plt.title(f'Transition matrix" - {timestamp}')
+    plt.xlabel('Next action')
+    plt.ylabel('Current action')
+    plt.tight_layout()
+    if infix is None:
+        plt.savefig(out_path / f"transition_matrix.svg")
+        plt.savefig(out_path / f"transition_matrix.png")
+    else:
+        plt.savefig(out_path / f"transition_matrix_{infix}.svg")
+        plt.savefig(out_path / f"transition_matrix_{infix}.png")
+    plt.show()
+
+    triple_transition_matrix = np.zeros((4, 2), dtype=int)
+    y_labels = ["00", "01", "10", "11"]
+    for i in range(len(A_history) - 2):
+        triple_transition_matrix[y_labels.index(f"{A_history[i]}{A_history[i+1]}"), A_history[i+2]] += 1
+
+    ax = sns.heatmap(triple_transition_matrix, annot=True, xticklabels=[0, 1], yticklabels=y_labels, cmap="Blues", fmt='d')
+    plt.title(f'Second-order transition matrix - {timestamp}')
+    plt.xlabel('Next action')
+    plt.ylabel('Current action')
+    plt.tight_layout()
+    if infix is None:
+        plt.savefig(out_path / f"second_order_transition_matrix.svg")
+        plt.savefig(out_path / f"second_order_transition_matrix.png")
+    else:
+        plt.savefig(out_path / f"second_order_transition_matrix_{infix}.svg")
+        plt.savefig(out_path / f"second_order_transition_matrix_{infix}.png")
+    plt.show()
