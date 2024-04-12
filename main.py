648031570a692ffc415b0168e0f8fd1df8b9867e
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-04-12 10:40:13 +0200

    Change window size to 20. Add more sleeping time.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ checkpoint = 0
 verbose = False
 checkers = False
 save = True
-msg = "Run LLM against Always-Defect strategy with sliding window of 15."
+msg = "Run LLM against Always-Defect strategy with sliding window of 20."
 
 if msg == "":
     log.info("Set a message.")
@@ -34,8 +34,9 @@ log.info(f"Starting time: {dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 print(f"Starting time: {dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 # Sleeping routine # TODO remove
+log.info("Going to sleep")
 print("Going to sleep")
-time.sleep(30000)
+time.sleep(60000)
 new_dt_start_time = dt.datetime.now()
 new_start_time = time.mktime(dt_start_time.timetuple())
 log.info(f"Starting time: {new_dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
