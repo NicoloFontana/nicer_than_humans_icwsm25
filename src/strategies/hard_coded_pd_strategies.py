8cb953747b1b12815694bc60227ed1b3c091e8e5
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-04-16 14:24:53 +0200

    Fix TitForTat strategy.

--- src/strategies/hard_coded_pd_strategies.py
+++ src/strategies/hard_coded_pd_strategies.py
@@ -1,12 +1,17 @@
+from src.games.gt_game import GTGame
 from src.strategies.strategy import Strategy
 
 
 class TitForTat(Strategy):
-    def __init__(self):
+    def __init__(self, game: GTGame, player_name: str):
+        self.game = game
+        self.player_name = player_name
         super().__init__("TitForTat")
 
-    def play(self, opponent_history=None, verbose=False):
-        if opponent_history is None:
+    def play(self):
+        opponent_name = self.game.get_opponents_names(self.player_name)[0]
+        opponent_history = self.game.get_actions_by_player(opponent_name)
+        if opponent_history is None or len(opponent_history) == 0:
             return 1
         return opponent_history[-1]
 
