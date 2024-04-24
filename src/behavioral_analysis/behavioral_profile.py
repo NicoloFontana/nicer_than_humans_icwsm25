fdf03f6128ad8c57261c6383f19f15db3b7b9f0c
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-04-24 09:39:16 +0200

    Add methods to add and compute features, and to save it to behavioral profile.

--- src/behavioral_analysis/behavioral_profile.py
+++ src/behavioral_analysis/behavioral_profile.py
@@ -1,5 +1,6 @@
 import json
 from src.behavioral_analysis.main_behavioral_features import main_behavioral_features
+from src.utils import OUT_BASE_PATH
 
 
 class BehavioralProfile:
@@ -9,6 +10,14 @@ class BehavioralProfile:
         self.n_games = None
         self.features = {}
 
+    def add_features(self, features):
+        for feature_name in features.keys():
+            self.features[feature_name] = features[feature_name]()
+
+    def compute_features(self, main_history, opponent_history):
+        for feature in self.features.values():
+            feature.compute_feature(main_history, opponent_history)
+
     def load_from_file(self, file_path, load_values=False):
         with open(file_path, "r") as profile_file:
             profile = json.load(profile_file)
@@ -17,3 +26,17 @@ class BehavioralProfile:
                 feature.load_from_file(file_path, load_values=load_values)
                 self.features[feature_name] = feature
             self.n_games = max(feature.n_games for feature in self.features.values())
+
+    def save_to_file(self, timestamp, subdir=None):
+        behavioral_profiles_dir = OUT_BASE_PATH / str(timestamp) / "behavioral_profiles"
+        dir_path = behavioral_profiles_dir if subdir is None else behavioral_profiles_dir / subdir
+        dir_path.mkdir(parents=True, exist_ok=True)
+        out_file_path = dir_path / f"behavioral_profile_{self.strategy_name}-{self.opponent_name}.json"
+        features_to_json = {}
+        for feature_name in self.features.keys():
+            feature = self.features[feature_name]
+            features_to_json[feature_name] = feature.to_json()
+        with open(out_file_path, "w") as out_file:
+            json_out = json.dumps(features_to_json, indent=4)
+            out_file.write(json_out)
+        return self.features
