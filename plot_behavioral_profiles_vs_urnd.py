fad2e1adb56fb0eede1d16da145bca67c18b2374
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-05-03 11:35:19 +0200

    Fix plot_behavioral_profiles_vs_urnd.py script.

--- plot_behavioral_profiles_vs_urnd.py
+++ plot_behavioral_profiles_vs_urnd.py
@@ -1,17 +1,21 @@
+from pathlib import Path
+
 from matplotlib import pyplot as plt
 import scipy.stats as st
 
 from src.behavioral_analysis.behavioral_profile import BehavioralProfile
-from src.strategies.strategy_utils import get_strategies, compute_behavioral_profile, get_strategy_by_name, plot_behavioral_profile, plot_errorbar
-from src.utils import OUT_BASE_PATH, compute_average_vector, out_path, timestamp
+from src.strategies.strategy_utils import plot_errorbar
 
-behavioral_profiles_path = OUT_BASE_PATH / "behavioral_profiles" / "llama2_vs_urnd"
-file_name = "behavioral_profile_Llama2-{}.json"
+
+base_path = Path("behavioral_profiles_analysis")
+strat_name = "llama2"
+strat_dir_path = base_path / strat_name
+
 cmap = plt.get_cmap('Dark2')
 confidence = 0.95
 features_analyzed = [
     "niceness",
-    "forgiveness2",
+    "forgiveness4",
     "provocability",
     "cooperativeness",
     "troublemaking",
@@ -23,27 +27,21 @@ features_analyzed = [
 # features_analyzed = main_behavioral_features.keys()
 
 def fmt_map(idx):
-    fmts = ['.', 'o', '|', '2', 's',
-            'p', 'h', '+', 'D', '^', 'x', '*']
+    fmts = ['.', 'o', '_', '2', 's',
+            'P', '^', '1', 'D', 'v', 'x', '*']
     return fmts[idx % len(fmts)]
 
 
 plt_fig = plt.figure()
 idx = 0
-for i in range(0, 9):
+for i in range(1, 9):
     coop_prob = i / 10
     coop_prob_str = str(coop_prob).replace(".", "")
-    if i != 0 and i != 5 and i != 10:
-        opponent_name = f"URND{coop_prob_str}"
-    elif i == 0:
-        opponent_name = "ALLD"
-    elif i == 5:
-        opponent_name = "RND"
-    else:
-        opponent_name = "ALLC"
-    label = f"vs {opponent_name}"
-    file_path = behavioral_profiles_path / file_name.format(opponent_name)
-    profile = BehavioralProfile("Llama2", opponent_name)
+    opponent_name = f"URND{coop_prob_str}"
+    urnd_dir_path = strat_dir_path / opponent_name
+    file_name = f"behavioral_profile_{strat_name}-{{}}.json"
+    file_path = urnd_dir_path / file_name.format(opponent_name)
+    profile = BehavioralProfile(strat_name, opponent_name)
     profile.load_from_file(file_path, load_values=True)
     means = []
     cis = []
@@ -56,14 +54,16 @@ for i in range(0, 9):
         cis.append(st.norm.interval(confidence, loc=feature.mean, scale=st.sem(feature.values)))
         yerrs.append((cis[-1][1] - cis[-1][0]) / 2)
     axhlines = [0.0, 0.5, 1.0]
-    plt_fig = plot_errorbar(means, cmap(idx), label, plt_figure=plt_fig, axhlines=axhlines, yerr=yerrs, fmt=fmt_map(idx))
+    label = f"vs {opponent_name}"
+    # plt_fig = plot_errorbar(means, cmap(idx), label, plt_figure=plt_fig, axhlines=axhlines, yerr=yerrs, fmt=fmt_map(idx))
+    plt_fig = plot_errorbar(means, cmap(idx), label, plt_figure=plt_fig, axhlines=axhlines, fmt=fmt_map(idx))
     idx += 1
 
 plt.figure(plt_fig)
 plt.ylabel("Level")
 plt.xlabel("Behavioral features")
 plt.xticks(range(len(features_analyzed)), features_analyzed, rotation=45, ha='right')
-plt.title(f"Llama2 behavioral profiles")
+plt.title(f"{strat_name} behavioral profiles")
 plt.legend(bbox_to_anchor=(1, 0.75))
 plt.tight_layout()
 
