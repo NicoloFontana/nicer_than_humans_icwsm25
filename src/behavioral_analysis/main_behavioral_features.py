efe25440e3c3594e9bce3603dfada111c8ad38cf
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-05-08 19:13:16 +0200

    Update behavioral features.

--- src/behavioral_analysis/main_behavioral_features.py
+++ src/behavioral_analysis/main_behavioral_features.py
@@ -23,103 +23,103 @@ class Niceness(BehavioralFeature):
         self.update_aggregates()
         return is_nice
 
+#
+# class Forgiveness1(BehavioralFeature):
+#     # 1-\frac{\sum(\frac{\mathsf{\#waited\_rounds}}{\mathsf{\#remaining\_rounds}})}{\mathsf{\#opponent\_defections}}
+#     def __init__(self):
+#         super().__init__("forgiveness1")
+#         self.values = []
+#         self.mean = None
+#         self.variance = None
+#         self.std_dev = None
+#
+#     def compute_feature(self, main_history: list, opponent_history: list) -> float:
+#         n = len(main_history)
+#         total_unforgiveness = 0
+#         opponent_defection = 0  # for each opponent's defection, there is a chance to forgive
+#         for i in range(n - 1):
+#             if opponent_history[i] == 0:  # opponent's defection
+#                 opponent_defection += 1
+#                 start = i + 1
+#                 forgiving_round = -1
+#                 for j in range(start, n):
+#                     if main_history[j] == 1:  # main cooperates after opponent's defection
+#                         forgiving_round = j
+#                         break
+#                 if forgiving_round == -1:
+#                     forgiving_round = n
+#                 total_unforgiveness += (forgiving_round - start) / (n - start) if n > start else 0  # ratio between waited rounds to cooperate and remaining rounds
+#                 # 0 if immediate cooperation, 1 if no cooperation
+#         relative_unforgiveness = total_unforgiveness / opponent_defection if opponent_defection > 0 else 0  # ratio between total unforgiveness and occasions to forgive
+#         # 0 if always forgives immediately, 1 if never forgives
+#         forgiveness = 1 - relative_unforgiveness
+#         self.values.append(forgiveness)
+#         self.update_aggregates()
+#         return forgiveness
+#
+#
+# class Forgiveness2(BehavioralFeature):
+#     # 1-\frac{\sum{\mathsf{\#waited\_rounds}}}{\sum{\mathsf{\#remaining\_rounds}}}
+#     def __init__(self):
+#         super().__init__("forgiveness2")
+#         self.values = []
+#         self.mean = None
+#         self.variance = None
+#         self.std_dev = None
+#
+#     def compute_feature(self, main_history: list, opponent_history: list) -> float:
+#         n = len(main_history)
+#         waited = 0  # how many rounds the main player waited to forgive in total
+#         remaining = 0  # how many rounds the main player had to forgive in total
+#         for i in range(n - 1):
+#             if opponent_history[i] == 0:  # opponent's defection
+#                 start = i + 1
+#                 forgiving_round = -1
+#                 for j in range(start, n):
+#                     if main_history[j] == 1:  # main cooperates after opponent's defection
+#                         forgiving_round = j
+#                         break
+#                 if forgiving_round == -1:
+#                     forgiving_round = n
+#                 waited += forgiving_round - start
+#                 remaining += n - start
+#         unforgiveness = waited / remaining if remaining > 0 else 0  # ratio between waited rounds to forgive and remaining rounds
+#         # 0 if always forgives immediately, 1 if never forgives
+#         forgiveness = 1 - unforgiveness
+#         self.values.append(forgiveness)
+#         self.update_aggregates()
+#         return forgiveness
+#
+#
+# class Forgiveness3(BehavioralFeature):
+#     def __init__(self):
+#         super().__init__("forgiveness3")
+#         self.values = []
+#         self.mean = None
+#         self.variance = None
+#         self.std_dev = None
+#
+#     def compute_feature(self, main_history: list, opponent_history: list) -> float:
+#         n = len(main_history)
+#         opponent_defection = 0  # for each opponent's defection, there is a chance to forgive
+#         unforgiven = 0
+#         for i in range(n):
+#             if main_history[i] == 1:  # main cooperates
+#                 unforgiven = max(0, unforgiven - 1)
+#             if opponent_history[i] == 0 and i < n - 1:  # opponent's defection
+#                 opponent_defection += 1
+#                 unforgiven += 1
+#         unforgiveness = unforgiven / opponent_defection if opponent_defection > 0 else 0  # ratio between total unforgiveness and occasions to forgive
+#         # 0 if always forgives immediately, 1 if never forgives
+#         forgiveness = 1 - unforgiveness
+#         self.values.append(forgiveness)
+#         self.update_aggregates()
+#         return forgiveness
 
-class Forgiveness1(BehavioralFeature):
-    # 1-\frac{\sum(\frac{\mathsf{\#waited\_rounds}}{\mathsf{\#remaining\_rounds}})}{\mathsf{\#opponent\_defections}}
+
+class Forgiveness(BehavioralFeature):
     def __init__(self):
-        super().__init__("forgiveness1")
-        self.values = []
-        self.mean = None
-        self.variance = None
-        self.std_dev = None
-
-    def compute_feature(self, main_history: list, opponent_history: list) -> float:
-        n = len(main_history)
-        total_unforgiveness = 0
-        opponent_defection = 0  # for each opponent's defection, there is a chance to forgive
-        for i in range(n - 1):
-            if opponent_history[i] == 0:  # opponent's defection
-                opponent_defection += 1
-                start = i + 1
-                forgiving_round = -1
-                for j in range(start, n):
-                    if main_history[j] == 1:  # main cooperates after opponent's defection
-                        forgiving_round = j
-                        break
-                if forgiving_round == -1:
-                    forgiving_round = n
-                total_unforgiveness += (forgiving_round - start) / (n - start) if n > start else 0  # ratio between waited rounds to cooperate and remaining rounds
-                # 0 if immediate cooperation, 1 if no cooperation
-        relative_unforgiveness = total_unforgiveness / opponent_defection if opponent_defection > 0 else 0  # ratio between total unforgiveness and occasions to forgive
-        # 0 if always forgives immediately, 1 if never forgives
-        forgiveness = 1 - relative_unforgiveness
-        self.values.append(forgiveness)
-        self.update_aggregates()
-        return forgiveness
-
-
-class Forgiveness2(BehavioralFeature):
-    # 1-\frac{\sum{\mathsf{\#waited\_rounds}}}{\sum{\mathsf{\#remaining\_rounds}}}
-    def __init__(self):
-        super().__init__("forgiveness2")
-        self.values = []
-        self.mean = None
-        self.variance = None
-        self.std_dev = None
-
-    def compute_feature(self, main_history: list, opponent_history: list) -> float:
-        n = len(main_history)
-        waited = 0  # how many rounds the main player waited to forgive in total
-        remaining = 0  # how many rounds the main player had to forgive in total
-        for i in range(n - 1):
-            if opponent_history[i] == 0:  # opponent's defection
-                start = i + 1
-                forgiving_round = -1
-                for j in range(start, n):
-                    if main_history[j] == 1:  # main cooperates after opponent's defection
-                        forgiving_round = j
-                        break
-                if forgiving_round == -1:
-                    forgiving_round = n
-                waited += forgiving_round - start
-                remaining += n - start
-        unforgiveness = waited / remaining if remaining > 0 else 0  # ratio between waited rounds to forgive and remaining rounds
-        # 0 if always forgives immediately, 1 if never forgives
-        forgiveness = 1 - unforgiveness
-        self.values.append(forgiveness)
-        self.update_aggregates()
-        return forgiveness
-
-
-class Forgiveness3(BehavioralFeature):
-    def __init__(self):
-        super().__init__("forgiveness3")
-        self.values = []
-        self.mean = None
-        self.variance = None
-        self.std_dev = None
-
-    def compute_feature(self, main_history: list, opponent_history: list) -> float:
-        n = len(main_history)
-        opponent_defection = 0  # for each opponent's defection, there is a chance to forgive
-        unforgiven = 0
-        for i in range(n):
-            if main_history[i] == 1:  # main cooperates
-                unforgiven = max(0, unforgiven - 1)
-            if opponent_history[i] == 0 and i < n - 1:  # opponent's defection
-                opponent_defection += 1
-                unforgiven += 1
-        unforgiveness = unforgiven / opponent_defection if opponent_defection > 0 else 0  # ratio between total unforgiveness and occasions to forgive
-        # 0 if always forgives immediately, 1 if never forgives
-        forgiveness = 1 - unforgiveness
-        self.values.append(forgiveness)
-        self.update_aggregates()
-        return forgiveness
-
-
-class Forgiveness4(BehavioralFeature):
-    def __init__(self):
-        super().__init__("forgiveness4")
+        super().__init__("forgiveness")
         self.values = []
         self.mean = None
         self.variance = None
@@ -266,7 +266,7 @@ class Consistency(BehavioralFeature):
         changes = 0
         for i in range(n - 1):
             changes += 1 if main_history[i] != main_history[i + 1] else 0
-        consistency = 1 - (changes / (n - 1)) if n > 1 else 0
+        consistency = 1 - (changes / (n - 1)) if n > 1 else 1
         self.values.append(consistency)
         self.update_aggregates()
         return consistency
@@ -274,10 +274,10 @@ class Consistency(BehavioralFeature):
 
 main_behavioral_features = {
     "niceness": Niceness,
-    "forgiveness1": Forgiveness1,
-    "forgiveness2": Forgiveness2,
-    "forgiveness3": Forgiveness3,
-    "forgiveness4": Forgiveness4,
+    # "forgiveness1": Forgiveness1,
+    # "forgiveness2": Forgiveness2,
+    # "forgiveness3": Forgiveness3,
+    "forgiveness": Forgiveness,
     "provocability": Provocability,
     "cooperativeness": Cooperativeness,
     "emulation": Emulation,
