56c96185417ca0069baf26ed7a666ffde073eb0f
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-05-30 10:36:22 +0200

    Run Llama3 against URNDx for x in [0.1, 0.5].

--- llm_main.py
+++ llm_main.py
@@ -23,7 +23,7 @@ n_games = 100
 n_iterations = 100
 checkpoint = 0
 checkers = False
-msg = "Run Llama3 vs AD with history window size 10."
+msg = "Run Llama3 vs URNDx. x in [0.1,0.5]. 100 games, 100 iterations, window size 10"
 
 if msg == "":
     log.info("Set a message.")
@@ -43,59 +43,59 @@ new_start_time = time.mktime(new_dt_start_time.timetuple())
 log.info(f"Starting time: {new_dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 print(f"Starting time: {new_dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 
-# for p in range(1, 11):  # TODO <--- b1
-#     coop_prob = p / 10
-#     urnd_str = f"URND{p:02}"
-#     print(f"URND{p:02}")
-for n_game in range(n_games):
-    log.info(f"Game {n_game + 1}") if n_games > 1 else None
-    print(f"Game {n_game + 1}") if n_games > 1 else None
-    checkers = [
-        TimeChecker(),
-        RuleChecker(),
-        AggregationChecker(),
-    ] if checkers else []
-    game = TwoPlayersPD(iterations=n_iterations)
-    game.add_player(Player(player_1_))
-    game.add_player(Player(player_2_))
+for p in range(1, 6):  # TODO <--- b1
+    coop_prob = p / 10
+    urnd_str = f"URND{p:02}"
+    print(f"URND{p:02}")
+    for n_game in range(n_games):
+        log.info(f"Game {n_game + 1}") if n_games > 1 else None
+        print(f"Game {n_game + 1}") if n_games > 1 else None
+        checkers = [
+            TimeChecker(),
+            RuleChecker(),
+            AggregationChecker(),
+        ] if checkers else []
+        game = TwoPlayersPD(iterations=n_iterations)
+        game.add_player(Player(player_1_))
+        game.add_player(Player(player_2_))
 
-    # TODO 2/3
-    ### HuggingFace client ###
-    client = InferenceClient(model=MODEL, token=HF_API_TOKEN)
-    client.headers["x-use-cache"] = "0"
+        # TODO 2/3
+        ### HuggingFace client ###
+        client = InferenceClient(model=MODEL, token=HF_API_TOKEN)
+        client.headers["x-use-cache"] = "0"
 
-    ### OpenAI client ###
-    # client = OpenAI(api_key=OPENAI_API_KEY)
+        ### OpenAI client ###
+        # client = OpenAI(api_key=OPENAI_API_KEY)
 
-    # TODO 3/3: check the checkers and the opponent's strategy (and coop prob)
-    strat1 = OneVsOnePDLlmStrategy(game, player_1_, client, history_window_size=history_window_size, checkers=checkers)
+        # TODO 3/3: check the checkers and the opponent's strategy (and coop prob)
+        strat1 = OneVsOnePDLlmStrategy(game, player_1_, client, history_window_size=history_window_size, checkers=checkers)
 
-    strat2 = AlwaysDefect()  # TODO <--- a
-    for player in game.players.values():
-        if player.get_name() == player_1_:
-            player.set_strategy(strat1)
-        else:
-            player.set_strategy(strat2)
-    for iteration in range(n_iterations):
-        curr_round = iteration + 1
-        log.info(f"Round {curr_round}") if n_games == 1 or curr_round % 10 == 0 else None
-        print(f"Round {curr_round}") if n_games == 1 or curr_round % 10 == 0 else None
-        if not game.is_ended:
-            game.play_round()
-            out_dir = OUT_BASE_PATH / {timestamp} if checkpoint != 0 and curr_round % checkpoint == 0 else None
-            # infix = f"{urnd_str}_{n_game + 1}_{curr_round}" if n_games > 1 else curr_round  # TODO <--- b2
-            infix = f"{n_game + 1}_{curr_round}" if n_games > 1 else curr_round
-            strat1.wrap_up_round(out_dir=out_dir, infix=infix)
-            log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}") if out_dir is not None else None
-            print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}") if out_dir is not None else None
-    out_dir = OUT_BASE_PATH / str(timestamp)
-    # infix = f"{urnd_str}_{n_game + 1}" if n_games > 1 else None  # TODO <--- b3 [END]
-    infix = f"{n_game + 1}" if n_games > 1 else None
-    strat1.wrap_up_round(out_dir=out_dir, infix=infix)
-    game.save_history(out_dir=out_dir, infix=infix)
+        strat2 = UnfairRandom(coop_prob)  # TODO <--- a
+        for player in game.players.values():
+            if player.get_name() == player_1_:
+                player.set_strategy(strat1)
+            else:
+                player.set_strategy(strat2)
+        for iteration in range(n_iterations):
+            curr_round = iteration + 1
+            log.info(f"Round {curr_round}") if n_games == 1 or curr_round % 10 == 0 else None
+            print(f"Round {curr_round}") if n_games == 1 or curr_round % 10 == 0 else None
+            if not game.is_ended:
+                game.play_round()
+                out_dir = OUT_BASE_PATH / {timestamp} if checkpoint != 0 and curr_round % checkpoint == 0 else None
+                infix = f"{urnd_str}_{n_game + 1}_{curr_round}" if n_games > 1 else curr_round  # TODO <--- b2
+                # infix = f"{n_game + 1}_{curr_round}" if n_games > 1 else curr_round
+                strat1.wrap_up_round(out_dir=out_dir, infix=infix)
+                log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}") if out_dir is not None else None
+                print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}") if out_dir is not None else None
+        out_dir = OUT_BASE_PATH / str(timestamp)
+        infix = f"{urnd_str}_{n_game + 1}" if n_games > 1 else None  # TODO <--- b3 [END]
+        # infix = f"{n_game + 1}" if n_games > 1 else None
+        strat1.wrap_up_round(out_dir=out_dir, infix=infix)
+        game.save_history(out_dir=out_dir, infix=infix)
 
-    # log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}")
-    # print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}")
+        # log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}")
+        # print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - start_time))}")
 
-    log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - new_start_time))}")
-    print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - new_start_time))}")
+        log.info(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - new_start_time))}")
+        print(f"Time elapsed: {dt.timedelta(seconds=int(time.time() - new_start_time))}")
