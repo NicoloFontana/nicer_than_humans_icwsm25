02cfb794ab7c5793fe64e737bd83d3b19ac62600
Nicolò Fontana <nicolo.fontana@mail.polimi.it>
2024-06-07 18:02:29 +0200

    Run Llama3 against WSLS.

--- llm_main.py
+++ llm_main.py
@@ -24,7 +24,7 @@ n_games = 100
 n_iterations = 100
 checkpoint = 0
 checkers = False
-msg = "Run Llama3 vs GRIM 100 games, 100 iterations, window size 10"
+msg = "Run Llama3 vs WSLS 100 games, 100 iterations, window size 10"
 
 if msg == "":
     log.info("Set a message.")
@@ -38,7 +38,7 @@ print(f"Starting time: {dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
 # Sleeping routine
 log.info("Going to sleep")
 print("Going to sleep")
-time.sleep(43200)
+time.sleep(86400)
 new_dt_start_time = dt.datetime.now()
 new_start_time = time.mktime(new_dt_start_time.timetuple())
 log.info(f"Starting time: {new_dt_start_time.strftime('%Y-%m-%d %H:%M:%S')}")
@@ -70,7 +70,7 @@ for n_game in range(n_games):
     game.add_player(llm_player)
 
     second_player = Player(player_2_)
-    second_player_strategy = Grim(game, player_2_)  # TODO <--- a
+    second_player_strategy = WinStayLoseShift(game, player_2_)  # TODO <--- a
     second_player.set_strategy(second_player_strategy)
     game.add_player(second_player)
 
